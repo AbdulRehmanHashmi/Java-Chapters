35523fb67ff8242738669b0ff6eb54ed482d03a1
AbdulRehmanHashmi <155216102+AbdulRehmanHashmi@users.noreply.github.com>
2024-02-24 04:03:08 +0500

    Add files via upload

---
+++ chapter1.js
@@ -0,0 +1,13 @@
+
+// Question 1
+alert("Assalam U Alaikum")
+// Question 2
+    alert("Error! Please enter a valid password.")
+    // Question 3
+    alert("Welcome to JS Land... \nHappy Coding!")
+    // Question 4
+    alert("Welcome to JS Land...")
+    alert("Happy Coding!")
+
+
+
